9b1c8fd792b80a7280e8b03098133de0f753a399
Jacob Hoffman <jackstudent149@gmail.com>
2021-02-21 01:48:37 -0500

    made lower side pane 200px

--- Demo.js
+++ Demo.js
@@ -37,7 +37,7 @@ function App(){
                         <Text s={"50px"}>Note 10 :)  </Text>
                     </R>
                     <R t>
-                        <Text s={"50px"}> Plain Pad Copy in Rectangles.js</Text>
+                        <Text s={"200px"}> Plain Pad Copy in Rectangles.js</Text>
                     </R>
                 </R>
 
