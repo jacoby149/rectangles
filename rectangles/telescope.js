6a9f54862f7124799bacd8eff76d27700b12b1e2
Jacob Hoffman <jackstudent149@gmail.com>
2021-02-23 17:27:27 -0500

    clean telescope

--- rectangles/telescope.js
+++ rectangles/telescope.js
@@ -1,11 +1,11 @@
 
-function dimDiff(div,nottel){
+function dimDiff(nottel){
     var [w,h]=[0,0];
     for (var i = 0; i < nottel.length; i++) {
         var child = nottel[i];
         [w,h]=[w+child.offsetWidth,h+child.offsetHeight];
     }
-    return [(div.clientWidth-w) + "px",(div.clientHeight-h) + "px"];
+    return ["calc(100% - " + w + "px)","calc(100% - " + h + "px)"];
 }
 
 function telescope(div){
@@ -15,7 +15,7 @@ function telescope(div){
     /* Do some telescoping */
     if (tel.length == 1){
         const t = tel[0];
-        const [dwidth,dheight] = dimDiff(div,nottel)
+        const [dwidth,dheight] = dimDiff(nottel)
         const side = div.getAttribute('childfloat');
         const h = side=="left" || side=="right";
         const v = side=="top" || side=="bottom";
