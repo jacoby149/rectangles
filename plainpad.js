9369129ad52040a95a3be5b308ab12436eaea661
Jacob Hoffman <jackstudent149@gmail.com>
2021-03-04 20:26:56 -0500

    oy vey grey artifacts...

--- plainpad.js
+++ plainpad.js
@@ -112,7 +112,7 @@ function Branding(props){
         <R l {...pass(props)}>
             
                 <C l ns s = {"70px"}>{/* Plain Pad Logo */}
-                    <img src = {"https://alextselegidis.com/try/plainpad-standalone/static/media/logo.af1c98b2.svg"} style={{height:"60%",margin:"20%"}} />
+                    <img src = {"https://alextselegidis.com/try/plainpad-standalone/static/media/logo.af1c98b2.svg"} style={{height:"60%",margin:"19%"}} />
                 </C>
                 
                 <C l ns mc s = {"120px"}>
