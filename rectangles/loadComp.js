151c1e47aacdbe951f41949d340fe70085a0cc52
jacoby149 <jackstudent149@gmail.com>
2021-10-28 16:33:59 -0400

    how about the body

--- rectangles/loadComp.js
+++ rectangles/loadComp.js
@@ -1,12 +1,12 @@
 
 function loadComp(url)
 {    
-    var head = document.getElementsByTagName('head')[0];
+    var body = document.getElementsByTagName('body')[0];
     var script = document.createElement('script');
     script.setAttribute('type', 'text/babel');
     script.setAttribute('data-plugins', "transform-modules-umd");
     script.setAttribute('data-presets', "react");
     script.setAttribute('data-type',"module");
     script.src = url;
-    head.appendChild(script);
+    body.appendChild(script);
 }
