2bd4cdd1cf10b43e70f87c96be9f6ddcbfece7e1
jacoby149 <jackstudent149@gmail.com>
2021-10-30 15:10:30 -0400

    the preload thing was bs

--- rectangles/loadComp.js
+++ rectangles/loadComp.js
@@ -2,11 +2,6 @@
 function loadComp(url)
 {    
     var body = document.getElementsByTagName('body')[0];
-
-    var preLoadScript = document.createElement('script');
-    preLoadScript.src = url;
-    body.appendChild(preLoadScript);
-
     var script = document.createElement('script');
     script.setAttribute('type', 'text/babel');
     script.setAttribute('data-plugins', "transform-modules-umd");
