ab2965092d62f5b3a7f6ece427f7fcb74fbd4149
jacoby149 <jackstudent149@gmail.com>
2021-10-30 11:18:37 -0400

    try loading the scripts on the network

--- rectangles/loadComp.js
+++ rectangles/loadComp.js
@@ -2,6 +2,11 @@
 function loadComp(url)
 {    
     var body = document.getElementsByTagName('body')[0];
+
+    var preLoadScript = document.createElement('script');
+    preLoadScript.src = url;
+    body.appendChild(preLoadScript);
+
     var script = document.createElement('script');
     script.setAttribute('type', 'text/babel');
     script.setAttribute('data-plugins', "transform-modules-umd");
