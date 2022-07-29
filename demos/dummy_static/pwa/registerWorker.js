1a510c658d9b224b411bf9febb108badc92151b2
jacoby149 <jackstudent149@gmail.com>
2022-07-29 17:05:30 -0400

    plainpad demo is the only needed pwa

--- demos/dummy_static/pwa/registerWorker.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("pwa/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
